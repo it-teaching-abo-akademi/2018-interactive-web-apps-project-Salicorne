6cd8bbf58b79cc517cf1bbbbba476516d650a4b6
Salicorne <timothee.Raboeuf@gmail.com>
2018-12-27 11:08:22 +0100

    Added production build

---
+++ docs/precache-manifest.b80b4e8dcb5695ddbaca4e00b5202181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70101089292e6586a3b3",
+    "url": "/2018-interactive-web-apps-project-Timothee-Raboeuf/static/js/runtime~main.70101089.js"
+  },
+  {
+    "revision": "e93fb017655769ad8fc2",
+    "url": "/2018-interactive-web-apps-project-Timothee-Raboeuf/static/js/main.e93fb017.chunk.js"
+  },
+  {
+    "revision": "31fb12a2aaafb08f13a9",
+    "url": "/2018-interactive-web-apps-project-Timothee-Raboeuf/static/js/1.31fb12a2.chunk.js"
+  },
+  {
+    "revision": "e93fb017655769ad8fc2",
+    "url": "/2018-interactive-web-apps-project-Timothee-Raboeuf/static/css/main.baa95cd0.chunk.css"
+  },
+  {
+    "revision": "9156b1f607b75b7f6e81ee948599b871",
+    "url": "/2018-interactive-web-apps-project-Timothee-Raboeuf/index.html"
+  }
+];
